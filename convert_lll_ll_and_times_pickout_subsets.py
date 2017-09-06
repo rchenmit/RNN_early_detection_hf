a94d549d1312923c3ff9fbed87ac05721d846d00
Robert Chen <robchen.mit@gmail.com>
2017-09-06 01:58:11 -0400

    update subsetting

--- convert_lll_ll_and_times_pickout_subsets.py
+++ convert_lll_ll_and_times_pickout_subsets.py
@@ -64,18 +64,23 @@ print "num patients 1: ", num_pts_1
 l_pcts_pick = [ 0.02, 0.035, 0.1, 0.2, 0.3,0.4,0.5,0.6,0.7,0.8,0.9]
 
 for pct in l_pcts_pick:
-	num_pick_0 = np.floor(pct * num_pts_0)
-	num_pick_1 = np.floor(pct * num_pts_1)
-	
+	num_pick_0 = int(np.floor(pct * num_pts_0))
+	num_pick_1 = int( np.floor(pct * num_pts_1))
+
 	l_idx_0 = np.random.choice(l_labels_0, num_pick_0, replace = False)
 	l_idx_1 = np.random.choice(l_labels_1, num_pick_1, replace = False)
+
+        # labels 
+        l_labels_new = []
 
 	# sequences
 	l_seqs_this = []
 	for idx in l_idx_0:
-		l_seqs_0_this.append(ll_seqs_new[idx])
+		l_seqs_this.append(ll_seqs_new[idx])
+                l_labels_new.append(0)
 	for idx in l_idx_1:
-		l_seqs_0_this.append(ll_seqs_new[idx])
+		l_seqs_this.append(ll_seqs_new[idx])
+                l_labels_new.append(1)
 
 	# times
 	l_times_this = []
@@ -84,14 +89,17 @@ for pct in l_pcts_pick:
 	for idx in l_idx_1:
 		l_times_this.append(ll_times[idx])	
 
+
 	# new save filenames
 	seqs_this_save_name = file_save_ll_seqs_new + '.subset_' + str(pct)
 	times_this_save_name = file_save_ll_t_new + '.subset_' + str(pct)
+        labels_this_save_name = file_l_labels + '.subset_' + str(pct)
 
 	# dump pickles
 	pickle.dump(ll_seqs_new, open(seqs_this_save_name, 'wb'))
 	pickle.dump(ll_times, open(times_this_save_name, 'wb'))
-
+	pickle.dump(l_labels_new, open(labels_this_save_name, 'wb'))
+        
 
 
 
